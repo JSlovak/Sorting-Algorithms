b1346ba08d5881cc49d7b30e6d1f8b6c0b7087f6
JSlovak <jasmine.slovak@gmail.com>
2017-04-06 20:41:10 -1000

    Merge sort coding, one bug with undefined at the beginning of the new array

--- js/merge.js
+++ js/merge.js
@@ -3,18 +3,31 @@ module.exports = function mergeSort(arr) {
   if(arr.length <= 1){
     return arr;
   }
+
 // Divide and conquer !!
+  function merge( R, L){
+    var pointR = 0;
+    var pointL = 0;
+    var outputArr = [];
+
+    if (L[0] > R[0]){
+      outputArr.push(pointR[0]);
+      pointR++;
+    } else
+      { outputArr.push(pointL[0]);
+        pointL++;
+      }
+    return outputArr;
+  }
+  // Divides the array in two
   var leftHalf = arr.slice(0, arr.length/2);
   var rightHalf = arr.slice(0,arr.length/2);
+
+  // Recurses on on each half of the array
   var leftSorted = mergeSort(leftHalf);
   var rightSorted = mergeSort(rightHalf);
 
-//Merge subarrays
+  //Merges subarrays
   return merge(leftSorted, rightSorted);
-
-  function merge( R, L){
-
-
-  }
 
 };
--- js/tests/merge.spec.js
+++ js/tests/merge.spec.js
@@ -2,7 +2,7 @@
 
 var chai = require("chai");
 var expect = chai.expect;
-var bubblesort = require("../merge");
+var mergeSort = require("../merge");
 
 var sortA = [35,45,5,40,50,25,10,15,20,30];
 var sortC = [26,6,8,14,20,4,10,12,2,16,18,22,24];
@@ -11,17 +11,17 @@ var sortC = [26,6,8,14,20,4,10,12,2,16,18,22,24];
 describe("merge-sort", function(){
 
   it("should be a function", function(){
-    expect(mergesort).to.be.a("function");
+    expect(mergeSort).to.be.a("function");
   });
 
   it("should return an array", function(){
-    expect(mergesort(sortA)).to.be.a("array");
+    expect(mergeSort(sortA)).to.be.a("array");
 
   });
 
   it("should return the array sorted to consecutive order (left to right)", function(){
-    expect(mergesort(sortA)).to.equal([5,10,15,20,25,30,35,40,45,50]);
-    expect(mergesort(sortC)).to.equal([2,4,6,8,10,12,14,16,18,20,22,24,26]);
+    expect(mergeSort(sortA)).to.equal([5,10,15,20,25,30,35,40,45,50]);
+    expect(mergeSort(sortC)).to.equal([2,4,6,8,10,12,14,16,18,20,22,24,26]);
 
   });
 });
